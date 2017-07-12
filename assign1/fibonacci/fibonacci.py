31faa262b7c97f856afda87154a34c572c001871
Vedarth Sharma <vedarth.sharma@gmail.com>
2017-07-12 19:32:16 +0530

    Fibonacci

--- assign1/fibonacci/fibonacci.py
+++ assign1/fibonacci/fibonacci.py
@@ -1,9 +1,17 @@
 # Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
+import sys
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
-
-n = int(input())
-print(calc_fib(n))
+def fib(n):
+    a=0
+    b=1
+    for i in range(n):
+        c = a + b
+        a = b
+        b = c
+        if i == n-1:
+            return c
+    
+input = sys.stdin.read()
+token = input.split()
+n = int(token[0])
+print(fib(n-1))
