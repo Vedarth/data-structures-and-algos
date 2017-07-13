9180692fd9f084558ff4bba27c2d1e54770bc666
Vedarth Sharma <vedarth.sharma@gmail.com>
2017-07-13 15:48:42 +0530

    Sum of fibonacci

--- assign1/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
+++ assign1/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
@@ -1,21 +1,31 @@
 # Uses python3
 import sys
 
-def fibonacci_sum_naive(n):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-    sum      = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current
-
-    return sum % 10
+def fib(n,m):
+    if n == -1:
+        return 0
+    if n== 0:
+        return 1
+    a=0
+    b=1
+    for i in range(n):
+        c = (a + b)%(m)
+        a = b
+        b = c
+        if i == n-1:
+            return c%m
+def pisano(m):
+    a=0
+    b=1
+    c=a+b
+    for i in range(m*m):
+        c=(a+b)%m
+        a,b=b,c
+        if a==0 and b==1:
+            return i+1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    print(fibonacci_sum_naive(n))
+    num = (n+2)%pisano(10)
+    print(fib(num-1,10)-1)
