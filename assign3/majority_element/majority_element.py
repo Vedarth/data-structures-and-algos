4e8c66f39088efcbc45fe6478d64eb3477e383f3
Vedarth <vedarth.sharma@gmail.com>
2017-08-25 20:50:23 +0530

    Update majority elements

--- assign3/majority_element/majority_element.py
+++ assign3/majority_element/majority_element.py
@@ -2,12 +2,13 @@
 import sys
 
 def get_majority_element(a, left, right):
-    if left == right:
+    num_count = dict()
+    for x in a:
+        num_count[x] = num_count.get(x,0) + 1
+    if max(num_count.values())>n//2:
+        return 1
+    else:
         return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
