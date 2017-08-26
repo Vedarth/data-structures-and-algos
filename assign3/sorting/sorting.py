8aee982d55d76a93b3a82e545702045dcd25e48e
Vedarth <vedarth.sharma@gmail.com>
2017-08-27 01:08:49 +0530

    sorting update

--- assign3/sorting/sorting.py
+++ assign3/sorting/sorting.py
@@ -3,8 +3,22 @@ import sys
 import random
 
 def partition3(a, l, r):
-    #write your code here
-    pass
+    x, j, t = a[l], l, r
+    i = j
+
+    while i <= t :
+       if a[i] < x:
+           a[j], a[i] = a[i], a[j]
+           j += 1
+
+       elif a[i] > x:
+           a[t], a[i] = a[i], a[t]
+           t -= 1
+           i -= 1 # remain in the same i in this case
+       i += 1   
+    return j, t
+
+
 
 def partition2(a, l, r):
     x = a[l]
@@ -23,9 +37,9 @@ def randomized_quick_sort(a, l, r):
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
     #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    m1, m2 = partition3(a, l, r)
+    randomized_quick_sort(a, l, m1 - 1);
+    randomized_quick_sort(a, m2 + 1, r);
 
 
 if __name__ == '__main__':
