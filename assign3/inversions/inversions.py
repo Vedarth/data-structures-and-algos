0e8f35dc93b91a8fd141e32bdfcd75687f44d4f2
Vedarth <vedarth.sharma@gmail.com>
2017-08-28 01:20:09 +0530

    add inversions

--- assign3/inversions/inversions.py
+++ assign3/inversions/inversions.py
@@ -1,18 +1,43 @@
 # Uses python3
 import sys
+def global_d():
+	global d
+	d = 0
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
-    ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+def merge(a,b):
+    """ Function to merge two arrays """
+    c = []
+    global d
+    while len(a) != 0 and len(b) != 0:
+        if a[0] < b[0]:
+            c.append(a[0])
+            a.remove(a[0])
+            d+=1
+        else:
+            c.append(b[0])
+            b.remove(b[0])
+    if len(a) == 0:
+        c += b
+    else:
+        c += a
+    return c
 
+# Code for merge sort
+
+def mergesort(x):
+    """ Function to sort an array using merge sort algorithm """
+    global d
+    if len(x) == 0 or len(x) == 1:
+        return x
+    else:
+        middle = len(x)//2
+        a = mergesort(x[:middle])
+        b = mergesort(x[middle:])
+        return merge(a,b)
 if __name__ == '__main__':
     input = sys.stdin.read()
+    global_d()
     n, *a = list(map(int, input.split()))
     b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    mergesort(a)
+    print(d//2)
