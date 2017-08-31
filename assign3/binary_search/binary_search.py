18c1a9847c414f35cc254e5864af4ea19e18358d
Vedarth <vedarth.sharma@gmail.com>
2017-09-01 03:32:10 +0530

    add print for understanding

--- assign3/binary_search/binary_search.py
+++ assign3/binary_search/binary_search.py
@@ -3,7 +3,25 @@ import sys
 
 def binary_search(a, x):
     left, right = 0, len(a)
-    # write your code here
+    while left<=right:
+        mid = left + (right-left)//2
+        try:
+            a[mid]
+        except:
+            return -1
+        else:
+            pass
+        finally:
+            pass
+        if a[mid] == x:
+            return mid
+        elif a[mid] < x:
+            left = mid+1
+        elif a[mid] > x:
+            right = mid-1
+        else:
+            return -1
+    return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -17,6 +35,6 @@ if __name__ == '__main__':
     n = data[0]
     m = data[n + 1]
     a = data[1 : n + 1]
+    a.sort()
     for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
--- assign3/inversions/inversions.py
+++ assign3/inversions/inversions.py
@@ -1,43 +1,43 @@
 # Uses python3
 import sys
-def global_d():
-	global d
-	d = 0
+count = 0
 
-def merge(a,b):
-    """ Function to merge two arrays """
-    c = []
-    global d
-    while len(a) != 0 and len(b) != 0:
-        if a[0] < b[0]:
-            c.append(a[0])
-            a.remove(a[0])
-            d+=1
-        else:
-            c.append(b[0])
-            b.remove(b[0])
-    if len(a) == 0:
-        c += b
-    else:
-        c += a
-    return c
+def inversionsCount(x):
+    global count
+    midsection = len(x) // 2
+    leftArray = x[:midsection]
+    rightArray = x[midsection:]
+    if len(x) > 1:
+        inversionsCount(leftArray)
+        inversionsCount(rightArray)
 
-# Code for merge sort
-
-def mergesort(x):
-    """ Function to sort an array using merge sort algorithm """
-    global d
-    if len(x) == 0 or len(x) == 1:
-        return x
-    else:
-        middle = len(x)//2
-        a = mergesort(x[:middle])
-        b = mergesort(x[middle:])
-        return merge(a,b)
+        i, j = 0, 0
+        a = leftArray; b = rightArray
+        for k in range(len(a) + len(b) + 1):
+            if a[i] <= b[j]:
+                x[k] = a[i]
+                i += 1
+                if i == len(a) and j != len(b):
+                    while j != len(b):
+                        k +=1
+                        x[k] = b[j]
+                        j += 1
+                    break
+            elif a[i] > b[j]:
+                x[k] = b[j]
+                print(len(a), a, i, b, j, x)
+                count += (len(a) - i)
+                j += 1
+                if j == len(b) and i != len(a):
+                    while i != len(a):
+                        k+= 1
+                        x[k] = a[i]
+                        i += 1                    
+                    break   
+    return x
 if __name__ == '__main__':
     input = sys.stdin.read()
-    global_d()
     n, *a = list(map(int, input.split()))
-    b = n * [0]
-    mergesort(a)
-    print(d//2)
+    b = a
+    inversionsCount(a)
+    print(count)
