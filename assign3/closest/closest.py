c8b15c11c0a09de151b4d1432ffcaf988b304667
Vedarth <vedarth.sharma@gmail.com>
2017-09-06 20:54:07 +0530

    closest update

--- assign3/closest/closest.py
+++ assign3/closest/closest.py
@@ -4,7 +4,17 @@ import math
 
 def minimum_distance(x, y):
     #write your code here
-    return 10 ** 18
+    dist = list()
+    for i in range(len(x)):
+        dist.append((x[i]**2 + y[i]**2)**0.5)
+    coord = list(zip(x,y,dist))
+    coord.sort()
+    distance = None
+    for i in range(len(coord)-1):
+        #if distance is None or distance > (dist[i+1]-dist[i]) :
+        #    distance = (dist[i+1]-dist[i])
+        if x
+    return distance
 
 if __name__ == '__main__':
     input = sys.stdin.read()
