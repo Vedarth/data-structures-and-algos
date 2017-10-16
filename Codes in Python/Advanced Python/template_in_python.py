c5f4e7bd1b59bda7c052797b1414914e2cea5519
Vedarth <vedarth.sharma@gmail.com>
2017-10-16 19:46:00 +0530

    Added delimiter and methods of template

--- Codes in Python/Advanced Python/template_in_python.py
+++ Codes in Python/Advanced Python/template_in_python.py
@@ -1,16 +1,19 @@
 from string import Template
 
+class MyTemplate(Template):
+    delimiter = "#"
+
 def Main():
     cart = []
     cart.append(dict(item="Coke", price=8, qty=2))
     cart.append(dict(item="Cake", price=12, qty=1))
     cart.append(dict(item="Pizza", price=32, qty=4))
 
-    t = Template("$qty x $item = $price")
+    t = MyTemplate("#qty x #item = #price")
     total = 0
     print("Cart")
     for data in cart:
-        print (t.substitute(data))
+        print (t.safe_substitute(data))
         total += data["price"]
         print("Total:",total)
 
