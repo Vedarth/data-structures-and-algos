5454648aacf279763d5b9265c91614818d955424
Vedarth <vedarth.sharma@gmail.com>
2017-10-17 17:55:41 +0530

    Add inheritance in manager class

--- Codes in Python/oop/creating_subclasses.py
+++ Codes in Python/oop/creating_subclasses.py
@@ -24,11 +24,33 @@ class Developer(Employee):
 		super().__init__(first, last, pay)#Or Employee.__init__(first, last, pay)
 		self.prog_lang = prog_lang
 
+class Manager(Employee):
+	def __init__(self, first, last, pay, employees=None):
+		super().__init__(first, last, pay)
+		if employees is None:
+			self.employees = []
+		else:
+			self.employees = employees
+
+	def add_emp(self, emp):
+		if emp not in self.employees:
+			self.employees.append(emp)
+
+	def remove_emp():
+		if emp in self.employees:
+			self.employees.remove(emp)
+
+	def print_emps(self):
+		for emp in self.employees:
+			print('-->', emp.fullname())
 
 dev_1 = Developer('Vedarth','Sharma',1000000, 'Python')
 dev_2 = Developer('Corey','Schafer',100000, 'Java')
 
-print(dev_1.fullname(), dev_1.prog_lang)
+mgr_1 = Manager('Sue','Su',90000,[dev_2])
+mgr_1.add_emp(dev_1)
+mgr_1.print_emps()
+#print(dev_1.fullname(), dev_1.prog_lang)
 
 #print(dev_1.pay)
 #dev_1.apply_raise()
