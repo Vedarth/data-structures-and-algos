cc28b1d36f2fa7a12aabbfc215e12ea18e7ccd26
Vedarth <vedarth.sharma@gmail.com>
2017-10-17 16:55:30 +0530

    Add Final class variables

--- Codes in Python/oop/class_variables.py
+++ Codes in Python/oop/class_variables.py
@@ -1,12 +1,15 @@
 class Employee:
-	"""docstring for Employee"""
-	raise_amount = 1.04
+	
+	num_of_emps = 0
+	raise_amount = 1.04	
+
 	def __init__(self, first,last,pay):
 		self.first = first
 		self.last = last
 		self.pay = pay
 		self.email = first.lower() + '.' + last.lower() + '@company.com'
 
+		Employee.num_of_emps += 1
 
 	def fullname():
 		return '{} {}'.format(self.first,self.last)
@@ -18,9 +21,7 @@ emp_1 = Employee('Vedarth','Sharma',1000000)
 emp_2 = Employee('Corey','Schafer',100000)
 Employee.raise_amount = 1.04
 emp_1.raise_amount=1.05
-print(emp_1.__dict__)
-print(Employee.raise_amount, Employee.__dict__)
 emp_2.apply_raise()
 emp_1.apply_raise()
-print(emp_2.pay, emp_1.pay)
+print(emp_2.pay, emp_1.pay, Employee.num_of_emps, emp_1.num_of_emps)
 
