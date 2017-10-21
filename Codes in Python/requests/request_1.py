a2974b9b14a4e1e9e75d8522c04a0982bf41bfb8
Vedarth <vedarth.sharma@gmail.com>
2017-10-21 23:55:27 +0530

    add headers

--- Codes in Python/requests/request_1.py
+++ Codes in Python/requests/request_1.py
@@ -3,6 +3,10 @@ import requests
 test = requests.get("https://www.coursera.org")
 
 
+
+hearders = ('Content-Type': 'application/x-www-from-urlencoded; charset-UTF-8', 'User-Agent':'Fiddler', 'Host': 'www.applebees.com', 'Content-Length': '146')
+
+
 outfile = 'C:/Users/vedar/data-structures-and-algos/Codes in Python/requests/test.txt'
 outfile = open(outfile,'w')
 outfile.write(str(test.text.encode('utf-8')))
